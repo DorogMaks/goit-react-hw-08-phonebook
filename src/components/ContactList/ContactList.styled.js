c7a0ff852f46b71e0140d72eab25e9794ac3c0aa
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-12-13 14:16:21 +0200

    update ContactList.styled

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-  display: block;
-  /* height: 450px; */
-  /* overflow: auto; */
+  /* display: block;
+  height: 450px;
+  overflow: auto; */
+
+  display: flex;
+  flex-direction: column;
+  max-height: calc(100vh - 260px);
+  overflow: auto;
 `;
